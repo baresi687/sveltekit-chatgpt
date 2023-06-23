34ba167b91350984846cbb5e9b8400ce13585a98
hreinn <hreinngy@gmail.com>
2023-06-23 21:34:04 +0200

    Moves input to bottom fixed position. Adds auto scroll to bottom on response container height change. Fixes input loosing focus when disabled while streaming.

--- src/components/ChatGPT.svelte
+++ src/components/ChatGPT.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { CodeBlock } from '@skeletonlabs/skeleton';
 	import { processTextAndCodeBlocks } from '../utils/processTextAndCodeBlocks.js';
+	import { onMount } from 'svelte';
 
 	let inputValue = '';
 	let chatResponseStream = [];
@@ -11,7 +12,22 @@
 	let isError = false;
 	let errorString = '';
 	let parsedLines = [];
+	let inputRef;
+	let chatResponseContainerRef;
 	const decoder = new TextDecoder('utf-8');
+
+	onMount(() => {
+		const resizeObserver = new ResizeObserver(() => {
+			window.scrollTo({
+				top: document.body.scrollHeight,
+				left: 0,
+				behavior: 'smooth'
+			});
+		});
+
+		resizeObserver.observe(chatResponseContainerRef);
+		return () => resizeObserver.unobserve(chatResponseContainerRef);
+	});
 
 	async function getChatResponse(data) {
 		const model = {
@@ -40,11 +56,10 @@
 				isStreaming = true;
 
 				chatResponses = [
+					...chatResponses,
 					{
-						message: data,
 						stream: []
-					},
-					...chatResponses
+					}
 				];
 
 				reader.read().then(function processText({ done, value }) {
@@ -57,12 +72,14 @@
 						.map((line) => JSON.parse(line));
 
 					chatResponseStream = [...processTextAndCodeBlocks(parsedLines, chatResponseStream)];
-					chatResponses[0].stream = [...chatResponseStream];
+					chatResponses[chatResponses.length - 1].stream = [...chatResponseStream];
 
 					if (done) {
 						inputValue = '';
+						chatResponses[chatResponses.length - 1].message = data;
 						chatResponseStream = [];
 						isStreaming = false;
+						regainFocus();
 						return;
 					}
 					return reader.read().then(processText);
@@ -89,77 +106,93 @@
 			getChatResponse(inputValue).then();
 		}
 	}
+
+	function regainFocus() {
+		setTimeout(() => {
+			inputRef.focus();
+		}, 1);
+	}
 </script>
 
-<div class="max-w-2xl my-14 mx-auto relative">
-	<form on:submit={handleChat} class="relative">
-		<input
-			bind:value={inputValue}
-			id="chat"
-			class="w-full h-10 indent-2.5 rounded text-zinc-900 font-semibold placeholder:font-normal placeholder:text-zinc-600 disabled:bg-gray-400"
-			type="text"
-			placeholder="Send a message"
-			disabled={isStreaming}
-		/>
-		{#if isStreaming}
-			<div class="absolute top-2.5 right-4 placeholder-circle w-5 animate-pulse" />
+<div class=" my-14">
+	<div
+		bind:this={chatResponseContainerRef}
+		class="relative container mx-auto px-4 max-w-3xl pb-[6rem] mb-44"
+	>
+		{#if chatResponses.length > 0}
+			{#each chatResponses as chatResponse}
+				<div class="whitespace-pre-line break-words rounded my-8 p-4 bg-slate-800 text-zinc-200">
+					{#each chatResponse.stream as stream}
+						{#if stream.code}
+							<CodeBlock language={stream.language} code={stream.code} />
+						{:else}
+							<p>{stream.text}</p>
+						{/if}
+					{/each}
+					{#if chatResponse.message}
+						<div class="text-sm mt-3 bg-slate-900 p-2 rounded w-fit">
+							Your message: <span class="font-semibold">{chatResponse.message}</span>
+						</div>
+					{/if}
+				</div>
+			{/each}
 		{/if}
-		{#if inputValue && !isStreaming}
-			<button on:click={handleChat} class="absolute top-3 right-4">
-				<svg
-					aria-label="Submit question"
-					class="pointer-events-none"
-					xmlns="http://www.w3.org/2000/svg"
-					width="15"
-					height="16"
-					viewBox="0 0 15 16"
-					><path
-						fill="#1d4ed8"
-						d="M12.49 7.14L3.44 2.27c-.76-.41-1.64.3-1.4 1.13l1.24 4.34c.05.18.05.36 0 .54l-1.24 4.34c-.24.83.64 1.54 1.4 1.13l9.05-4.87a.98.98 0 0 0 0-1.72Z"
-					/></svg
-				>
-			</button>
-		{/if}
-	</form>
-	{#if isLoading}
-		<div class="absolute top-[4.5rem] bg-skeletonDark z-10 shadow rounded-md w-full mx-auto">
-			<div class="animate-pulse flex space-x-4">
-				<div class="flex-1 space-y-6 pb-4">
-					<div class="h-2 bg-slate-700 rounded" />
-					<div class="h-2 bg-slate-700 rounded" />
-					<div class="space-y-3">
-						<div class="grid grid-cols-3 gap-4">
-							<div class="h-2 bg-slate-700 rounded col-span-2" />
-							<div class="h-2 bg-slate-700 rounded col-span-1" />
+		{#if isLoading}
+			<div
+				class="absolute bottom-2 left-0 bg-transparent z-10 shadow rounded-md w-full mx-auto px-4"
+			>
+				<div class="animate-pulse flex space-x-4">
+					<div class="flex-1 space-y-6 pb-4">
+						<div class="h-2 bg-slate-700 rounded" />
+						<div class="h-2 bg-slate-700 rounded" />
+						<div class="space-y-3">
+							<div class="grid grid-cols-3 gap-4">
+								<div class="h-2 bg-slate-700 rounded col-span-2" />
+								<div class="h-2 bg-slate-700 rounded col-span-1" />
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	{/if}
-	{#if chatResponses.length > 0}
-		{#each chatResponses as chatResponse}
-			<div class="whitespace-pre-line rounded my-8 p-4 bg-slate-800 text-zinc-200">
-				{#each chatResponse.stream as stream}
-					{#if stream.code}
-						<CodeBlock language={stream.language} code={stream.code} />
-					{:else}
-						<p>{stream.text}</p>
-					{/if}
-				{/each}
-				{#if !isStreaming}
-					<div class="text-sm mt-3 bg-slate-900 p-2 rounded w-fit">
-						Your message: <span class="font-semibold">{chatResponse.message}</span>
-					</div>
-				{/if}
+		{/if}
+		{#if isError}
+			<div class="absolute bottom-0 left-0 z-10 w-full px-4">
+				<div class="bg-red-900 whitespace-pre-line rounded my-8 p-4 text-zinc-200">
+					{errorString}
+				</div>
 			</div>
-		{/each}
-	{/if}
-	{#if isError}
-		<div class="absolute top-10 min-h-screen z-10 bg-transparent w-full">
-			<div class="bg-red-900 whitespace-pre-line rounded my-8 p-4 text-zinc-200">
-				{errorString}
-			</div>
-		</div>
-	{/if}
+		{/if}
+	</div>
+	<div class="fixed bottom-0 z-50 w-full pt-10 pb-14 mt-4 bg-slate-800">
+		<form on:submit={handleChat} class="relative max-w-3xl mx-auto px-4">
+			<input
+				bind:this={inputRef}
+				bind:value={inputValue}
+				id="chat"
+				class="w-full h-10 indent-2.5 rounded text-zinc-900 font-semibold placeholder:font-normal placeholder:text-zinc-600 disabled:bg-gray-400"
+				type="text"
+				placeholder="Send a message"
+				disabled={isStreaming}
+			/>
+			{#if isStreaming}
+				<div class="absolute top-2.5 right-6 placeholder-circle w-5 animate-pulse" />
+			{/if}
+			{#if inputValue && !isStreaming}
+				<button on:click={handleChat} class="absolute top-3 right-6">
+					<svg
+						aria-label="Submit question"
+						class="pointer-events-none"
+						xmlns="http://www.w3.org/2000/svg"
+						width="15"
+						height="16"
+						viewBox="0 0 15 16"
+						><path
+							fill="#1d4ed8"
+							d="M12.49 7.14L3.44 2.27c-.76-.41-1.64.3-1.4 1.13l1.24 4.34c.05.18.05.36 0 .54l-1.24 4.34c-.24.83.64 1.54 1.4 1.13l9.05-4.87a.98.98 0 0 0 0-1.72Z"
+						/></svg
+					>
+				</button>
+			{/if}
+		</form>
+	</div>
 </div>
