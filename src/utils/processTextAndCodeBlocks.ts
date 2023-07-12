b50e2bb93544fc0d05ab27159171230fdb6a76b5
hreinn <hreinngy@gmail.com>
2023-07-12 15:06:32 +0200

    Refactors processTextAndCodeBlocks function

--- src/utils/processTextAndCodeBlocks.ts
+++ src/utils/processTextAndCodeBlocks.ts
@@ -1,3 +1,5 @@
+import { findIndex } from './functions';
+
 let isCode = false;
 let codeBlockRef = '';
 let textBlockRef = '';
@@ -15,70 +17,66 @@ interface IArr {
 }
 export function processTextAndCodeBlocks(stream: IStream[], arr: IArr[]) {
 	for (const elem of stream) {
-		const { choices, done }: IStream = elem;
+		const { choices, done } = elem;
 
-		if (choices) {
+		if (choices && choices[0].delta.content) {
 			const content = choices[0].delta.content;
+			streamString += content;
+			const isCodeString = streamString.trim().match(/\n```$|^```$/g);
 
-			if (content) {
-				streamString += content;
-				const isCodeString = streamString.trim().match(/\n```$|^```$/g);
-				if (isCodeString) {
-					isCode = !isCode;
-					if (isCode) {
-						codeBlockRef = 'codeRef' + Math.random().toString().slice(3, 9);
-						arr.push({
-							[codeBlockRef]: 'code',
-							code: '',
-							language: 'plaintext'
-						});
-					} else {
-						textBlockRef = 'textRef' + Math.random().toString().slice(3, 9);
-						arr.push({
-							[textBlockRef]: 'text',
-							text: ''
-						});
-					}
-				}
-
+			if (isCodeString) {
+				isCode = !isCode;
 				if (isCode) {
-					const findIt = arr.findIndex((item) => item[codeBlockRef as keyof IArr]);
-					const codeBackTicks = streamString.match(/```[a-z]*$/);
-
-					arr[findIt].code += content;
-
-					if (codeBackTicks) {
-						const codeLanguage = codeBackTicks.map((item) => item.slice(3, item.length));
-						if (codeLanguage[codeLanguage.length - 1].length > 1) {
-							arr[findIt].code = arr[findIt].code?.replace(
-								codeLanguage[codeLanguage.length - 1],
-								''
-							);
-						}
-						arr[findIt].language = codeLanguage[codeLanguage.length - 1].length
-							? codeLanguage[codeLanguage.length - 1]
-							: 'plaintext';
-					}
-
-					arr[findIt].code = arr[findIt].code?.replace(/^```|``$|^`\n$/, '');
+					codeBlockRef = 'codeRef' + Math.random().toString().slice(3, 9);
+					arr.push({
+						[codeBlockRef]: 'code',
+						code: '',
+						language: 'plaintext'
+					});
 				} else {
-					const findIt = arr.findIndex((item) => item[textBlockRef as keyof IArr]);
+					textBlockRef = 'textRef' + Math.random().toString().slice(3, 9);
+					arr.push({
+						[textBlockRef]: 'text',
+						text: ''
+					});
+				}
+			}
 
-					if (findIt !== -1) {
-						arr[findIt].text += content;
-						arr[findIt].text = arr[findIt].text?.replace(/^`\n|\s``$/, '');
-					} else {
-						textBlockRef = 'textRef' + Math.random().toString().slice(3, 9);
-						arr.push({
-							[textBlockRef]: 'text',
-							text: content
-						});
-						arr[arr.length - 1].text = arr[arr.length - 1].text?.replace(/``$/, '');
+			if (isCode) {
+				const findIt = findIndex(arr, codeBlockRef);
+				const codeBackTicks = streamString.match(/```[a-z]*$/);
+
+				arr[findIt].code += content;
+
+				if (codeBackTicks) {
+					const codeLanguage = codeBackTicks.map((item) => item.slice(3, item.length));
+
+					if (codeLanguage[codeLanguage.length - 1].length > 1) {
+						arr[findIt].code = arr[findIt].code?.replace(codeLanguage[codeLanguage.length - 1], '');
 					}
+					arr[findIt].language = codeLanguage[codeLanguage.length - 1].length
+						? codeLanguage[codeLanguage.length - 1]
+						: 'plaintext';
 				}
 
-				arr = [...arr.filter((item) => item.text !== '```')];
+				arr[findIt].code = arr[findIt].code?.replace(/^```|``$|^`\n$/, '');
+			} else {
+				const findIt = findIndex(arr, textBlockRef);
+
+				if (findIt !== -1) {
+					arr[findIt].text += content;
+					arr[findIt].text = arr[findIt].text?.replace(/^`\n|\s``$/, '');
+				} else {
+					textBlockRef = 'textRef' + Math.random().toString().slice(3, 9);
+					arr.push({
+						[textBlockRef]: 'text',
+						text: content
+					});
+					arr[arr.length - 1].text = arr[arr.length - 1].text?.replace(/``$/, '');
+				}
 			}
+
+			arr = [...arr.filter((item) => item.text !== '```')];
 		}
 		if (done) {
 			streamString = '';
@@ -86,3 +84,5 @@ export function processTextAndCodeBlocks(stream: IStream[], arr: IArr[]) {
 	}
 	return arr;
 }
+
+export type { IArr };
