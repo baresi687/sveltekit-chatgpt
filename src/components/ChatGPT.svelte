1a3ecf101e2b07d8a1c6c8cf0ea3789ee63895a4
hreinn <hreinngy@gmail.com>
2023-08-03 12:06:18 +0200

    Adds icon to error element

--- src/components/ChatGPT.svelte
+++ src/components/ChatGPT.svelte
@@ -279,6 +279,17 @@
 					<div
 						class="flex gap-4 items-center bg-red-900 whitespace-pre-line rounded mt-6 mb-8 p-4 text-zinc-200"
 					>
+						<svg
+							class="pointer-events-none shrink-0 self-start"
+							xmlns="http://www.w3.org/2000/svg"
+							width="32"
+							height="32"
+							viewBox="0 0 24 24"
+							><path
+								fill="#e4e4e7"
+								d="M12 17q.425 0 .713-.288T13 16q0-.425-.288-.713T12 15q-.425 0-.713.288T11 16q0 .425.288.713T12 17Zm-1-4h2V7h-2v6Zm1 9q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22Zm0-2q3.35 0 5.675-2.325T20 12q0-3.35-2.325-5.675T12 4Q8.65 4 6.325 6.325T4 12q0 3.35 2.325 5.675T12 20Zm0-8Z"
+							/></svg
+						>
 						<p>{errorString}</p>
 						{#if isLimitReached}
 							<button on:click={() => location.reload()} class="btn btn-sm bg-red-800"
