0ece7fb1ac2944657459f2e55d4bae8b07a27088
hreinn <hreinngy@gmail.com>
2023-08-08 11:25:21 +0200

    Sets fixed x position of scroll to bottom button on widths over 1024 px

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -47,25 +47,28 @@
 		<h1 class="text-center text-5xl px-4 mt-36">SvelteKit ChatGPT</h1>
 		<div bind:this={chatContainerRef}>
 			<ChatGPT />
-			<button
-				on:click={() => mainRef.scrollTo({ top: mainRef.scrollHeight, left: 0, behavior: 'auto' })}
-				aria-label="Scroll to bottom"
-				type="button"
-				class={`absolute bottom-40 left-[26px] ease-in-out duration-200 ${
-					isScroll ? 'opacity-0 invisible' : 'opacity-100 visible'
-				} md:right-7 md:left-auto rounded-full h-11 w-11 mb-0.5 flex justify-center items-center border-2 border-slate-800 bg-slate-900 hover:brightness-125`}
-				><svg
-					class="pointer-events-none"
-					xmlns="http://www.w3.org/2000/svg"
-					width="24"
-					height="24"
-					viewBox="0 0 24 24"
-					><path
-						fill="#cbd5e1"
-						d="M11 4h2v12l5.5-5.5l1.42 1.42L12 19.84l-7.92-7.92L5.5 10.5L11 16V4Z"
-					/></svg
-				></button
-			>
+			<div class="absolute inset-0 max-w-5xl pointer-events-none my-0 mx-auto">
+				<button
+					on:click={() =>
+						mainRef.scrollTo({ top: mainRef.scrollHeight, left: 0, behavior: 'auto' })}
+					aria-label="Scroll to bottom"
+					type="button"
+					class={`absolute bottom-40 left-[26px] ease-in-out duration-200 ${
+						isScroll ? 'opacity-0 invisible' : 'opacity-100 visible'
+					} md:right-7 md:left-auto pointer-events-auto rounded-full h-11 w-11 mb-0.5 flex justify-center items-center border-2 border-slate-800 bg-slate-900 hover:brightness-125`}
+					><svg
+						class="pointer-events-none"
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						><path
+							fill="#cbd5e1"
+							d="M11 4h2v12l5.5-5.5l1.42 1.42L12 19.84l-7.92-7.92L5.5 10.5L11 16V4Z"
+						/></svg
+					></button
+				>
+			</div>
 		</div>
 	</section>
 </main>
