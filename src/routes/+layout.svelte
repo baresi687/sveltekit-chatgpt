44588f128c39b7c246b5845ccdd6b0a20ac97a72
Hreinn Gylfason <hreinngy@gmail.com>
2024-03-07 09:56:55 +0100

    fix: tailwind/skeleton updated config after dependabot pr

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,15 +2,7 @@
 	import hljs from 'highlight.js';
 	import 'highlight.js/styles/github-dark.css';
 	import { storeHighlightJs } from '@skeletonlabs/skeleton';
-	storeHighlightJs.set(hljs);
-
-	// Your selected Skeleton theme:
-	import '@skeletonlabs/skeleton/themes/theme-crimson.css';
-
-	// This contains the bulk of Skeletons required styles:
-	import '@skeletonlabs/skeleton/styles/skeleton.css';
-
-	// Finally, your application's global stylesheet (sometimes labeled 'app.css')
+	storeHighlightJs.set(hljs);	
 	import '../app.postcss';
 </script>
 
